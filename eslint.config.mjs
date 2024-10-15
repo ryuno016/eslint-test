9f31a6d5087aff00734eb52828ea9ac665987f96
多多納 龍之介 <ryuno8901398@gmail.com>
2024-10-15 15:15:48 +0900

    エラーが出ないように修正

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,23 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import pluginReact from 'eslint-plugin-react';
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  { 
+    files: ['**/*.{js,mjs,cjs,jsx}'], 
+    languageOptions: { 
+      globals: globals.browser 
+    }, 
+    settings: { 
+      react: { 
+        version: 'detect' 
+      }
+    },
+    rules: {
+      quotes: ['error', 'single'], // ダブルクォートをシングルクォートに変更
+      semi: ['error', 'always'],   // セミコロンを常に追加
+    }
+  },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
 ];
